605537948350c0418e33db961fed0ead668db8cc
Thiago Hecksher <thiago.hecksher@gmail.com>
2013-05-27 21:49:59 -0300

    Adição de Imagens e personalização do estilo das sessões

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,9 @@
 if(!Modernizr.backgroundsize) {
   $('.hero-unit').backstretch('images/hero-bg.jpg');
+  $('#contact').backstretch('images/contact-bg.jpg');
 }
+
+$('a').smoothScroll();
+
+$('.navbar').scrollspy();
+
