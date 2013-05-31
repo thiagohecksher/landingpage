336b987f8b6a2a95fac5315411e74647a99305d3
Thiago Hecksher <thiago.hecksher@gmail.com>
2013-05-31 15:48:13 -0300

    adicionei Isotope

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,31 @@ $('a').smoothScroll();
 
 $('.navbar').scrollspy();
 
+var $container = $('.wrapper');
+
+$container.isotope({
+  itemSelector : '.element',
+  masonry : {
+    columnWidth : 155
+  },
+  masonryHorizontal : {
+    rowHeight: 155
+  },
+  cellsByRow : {
+    columnWidth : 300,
+    rowHeight : 300
+  },
+  cellsByColumn : {
+    columnWidth : 300,
+    rowHeight : 300
+  }
+});
+
+$('#filters a').click(function(e){
+  e.preventDefault();
+
+  var selector = $(this).attr('data-filter');
+  $container.isotope({ filter: selector });
+});
+
+$('.dropdown-toggle').dropdown()
